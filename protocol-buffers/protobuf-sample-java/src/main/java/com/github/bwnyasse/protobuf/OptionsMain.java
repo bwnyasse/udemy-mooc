afe2989bc7079ad93e5febb0d3791ca51a219feb
bwnyasse <risbonyasse@gmail.com>
2019-05-18 09:00:05 +0200

    Options & JSON for debugging

---
+++ protocol-buffers/protobuf-sample-java/src/main/java/com/github/bwnyasse/protobuf/OptionsMain.java
@@ -0,0 +1,11 @@
+package com.github.bwnyasse.protobuf;
+
+import com.github.bwnyasse.protobuf.options.OptionMessageOther;
+
+public class OptionsMain
+{
+
+	public static void main(String[] args) {
+		OptionMessageOther other = OptionMessageOther.newBuilder().build();
+	}
+}
