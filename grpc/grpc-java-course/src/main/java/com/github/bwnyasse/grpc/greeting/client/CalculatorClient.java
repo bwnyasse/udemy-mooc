5c995beb7353572a9700ba65e1739ffa0d87f5fd
bwnyasse <risbonyasse@gmail.com>
2019-05-25 08:34:33 +0200

    sample client streaming with compute average

--- grpc/grpc-java-course/src/main/java/com/github/bwnyasse/grpc/greeting/client/CalculatorClient.java
+++ grpc/grpc-java-course/src/main/java/com/github/bwnyasse/grpc/greeting/client/CalculatorClient.java
@@ -4,27 +4,37 @@ import com.proto.calculator.*;
 import com.proto.calculator.CalculatorServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
-import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CalculatorClient {
 
     public static void main(String[] args) {
         System.out.println("Hello Calculator gRPC client");
 
+        CalculatorClient client = new CalculatorClient();
+        client.run();
+    }
+
+    private void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext() // No for PROD
                 .build();
 
-        // create the client
-        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient = CalculatorServiceGrpc.newBlockingStub(channel);
-        sumRPCCall(calculatorClient);
-        primedecompoRPCCall(calculatorClient);
+
+        doSumCall(channel);
+        doPrimeDecompoCall(channel);
+        doComputeAverage(channel);
 
         channel.shutdown();
     }
 
-    private static void sumRPCCall(CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient) {
+    private void doSumCall(ManagedChannel channel) {
+
+        // create the client
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient = CalculatorServiceGrpc.newBlockingStub(channel);
 
         int first = 3;
         int second = 10;
@@ -34,13 +44,16 @@ public class CalculatorClient {
                 .setFirst(first)
                 .setSecond(second)
                 .build();
-        System.out.println(" SUM : " + first +" & " + second + " ----");
+        System.out.println(" SUM : " + first + " & " + second + " ----");
         // call The RCP , send the request & get the response
         ToSumResponse response = calculatorClient.sum(request);
         System.out.println(response.getResult());
     }
 
-    private static void primedecompoRPCCall(CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient) {
+    private void doPrimeDecompoCall(ManagedChannel channel) {
+
+        // create the client
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient = CalculatorServiceGrpc.newBlockingStub(channel);
 
         int entry = 120;
         PrimeDecompoRequest request = PrimeDecompoRequest.newBuilder()
@@ -54,5 +67,49 @@ public class CalculatorClient {
         });
 
 
+    }
+
+    private void doComputeAverage(ManagedChannel channel) {
+
+        System.out.println(" COMPUTE AVERAGE  ----");
+
+        CalculatorServiceGrpc.CalculatorServiceStub asyncClient = CalculatorServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<ComputeAverageRequest> requestObserver = asyncClient.computeAverage(new StreamObserver<ComputeAverageResponse>() {
+            @Override
+            public void onNext(ComputeAverageResponse value) {
+                // We get a response from the server
+                System.out.println("Received a response from the server");
+                System.out.println(value.getResult());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                // the server is done sending us data
+                System.out.println("Server has completed sending us something");
+                latch.countDown();
+            }
+        });
+
+        System.out.println("Entries are : ");
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i);
+            requestObserver.onNext(ComputeAverageRequest.newBuilder().setEntry(i).build());
+        }
+
+        requestObserver.onCompleted();;
+
+        try {
+            latch.await(3L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- grpc/grpc-java-course/src/main/java/com/github/bwnyasse/grpc/greeting/server/CalculatorServiceImpl.java
+++ grpc/grpc-java-course/src/main/java/com/github/bwnyasse/grpc/greeting/server/CalculatorServiceImpl.java
@@ -4,6 +4,10 @@ import com.proto.calculator.*;
 import com.proto.calculator.CalculatorServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
 public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
 
     @Override
@@ -21,9 +25,41 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
     @Override
     public void primeDecompo(PrimeDecompoRequest request, StreamObserver<PrimeDecompoResponse> responseObserver) {
 
-        primedecompoAlgo(request.getEntry(),responseObserver);
+        primedecompoAlgo(request.getEntry(), responseObserver);
         responseObserver.onCompleted();
     }
+
+    @Override
+    public StreamObserver<ComputeAverageRequest> computeAverage(StreamObserver<ComputeAverageResponse> responseObserver) {
+
+        return new StreamObserver<ComputeAverageRequest>() {
+
+            List<Integer> listOfValues = new ArrayList<>();
+
+
+            @Override
+            public void onNext(ComputeAverageRequest value) {
+                listOfValues.add(value.getEntry());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                OptionalDouble optionalDouble = listOfValues.stream().mapToInt(a -> a).average();
+                Double average = optionalDouble.isPresent() ? optionalDouble.getAsDouble() : 0;
+
+                responseObserver.onNext(ComputeAverageResponse.newBuilder()
+                        .setResult(average)
+                        .build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
 
     private void primedecompoAlgo(int entry, StreamObserver<PrimeDecompoResponse> responseObserver) {
         int k = 2;
